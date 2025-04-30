9521bdd1b92fe965421c1ba1c4b4df0f1018fc6d
Douglas Silva <douglasitpro@gmail.com>
2025-04-30 14:05:21 -0300

    feat: criando logica de views para administração do blog

--- app_blog/views.py
+++ app_blog/views.py
@@ -5,13 +5,15 @@ from django.db.models import Q     #busca complexa
 from django.shortcuts import render, get_object_or_404 # retorna detalhes de um objeto especifico
 from . forms import PostForm
 from django.http import HttpResponseForbidden
+from django.contrib.auth.decorators import user_passes_test
+
 
 
 def post_list(request):
 
     query = request.GET.get('q', '')  # captura o termo da busca (ex: ?q=palavra), valor padrão vazio '' evita exibir 'None'
     
-    posts = Post.objects.filter(published_date__lte=timezone.now())
+    posts = Post.objects.filter(aprovado=True, published_date__lte=timezone.now())
 
     if query:
         posts = posts.filter(
@@ -61,3 +63,49 @@ def post_edit(request, pk):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk, published_date__lte=timezone.now())  # para evitar que usuários vejam posts ainda não publicados.
     return render(request, 'app_blog/post_detail.html', {'post': post })
+
+
+
+#-----------------Administração do blog-----------------------
+
+def is_admin(user):
+    return user.is_authenticated and user.is_staff
+
+@user_passes_test(is_admin)
+def painel_administrativo(request):
+    posts_pendentes = Post.objects.filter(aprovado=False, recusado=False)
+    return render(request, 'app_blog/painel_admin.html', {'posts_pendentes': posts_pendentes})
+
+@user_passes_test(is_admin)
+def aprovar_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.aprovado = True
+    post.published_date = timezone.now()
+    post.save()
+    return redirect('painel_administrativo')
+
+@user_passes_test(is_admin)
+def excluir_post_admin(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('painel_administrativo')
+
+@user_passes_test(is_admin)
+def recusar_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.recusado = True
+    post.save()
+    return redirect('painel_administrativo')
+
+@user_passes_test(is_admin)
+def editar_post_admin(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.save()
+            return redirect('painel_administrativo')
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'app_blog/post_edit.html', {'form': form, 'post': post})
