a66de203ce6bf080c7d756cfc85fc1d68651bf74
Douglas Silva <douglasitpro@gmail.com>
2025-04-29 22:30:19 -0300

    feat: configurando a model no admin do django com recurso mais avançadas

--- app_blog/admin.py
+++ app_blog/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from django.utils import timezone
+from .models import Post
 
-# Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'created_date', 'published_date', 'is_published')
+    list_filter = ('author', 'published_date', 'created_date')
+    search_fields = ('title', 'text', 'author__username')
+    readonly_fields = ('created_date', 'published_date')
+    actions = ['publicar_posts', 'despublicar_posts']
+
+    fieldsets = (
+        (None, {
+            'fields': ('title', 'author', 'text')
+        }),
+        ('Publicação', {
+            'fields': ('created_date', 'published_date'),
+            'classes': ('collapse',),
+        }),
+    )
+
+    @admin.display(boolean=True, description="Publicado?")
+    def is_published(self, obj):
+        return obj.published_date is not None
+
+    def publicar_posts(self, request, queryset):
+        count = queryset.update(published_date=timezone.now())
+        self.message_user(request, f"{count} post(s) publicado(s) com sucesso.")
+    publicar_posts.short_description = "Publicar posts selecionados"
+
+    def despublicar_posts(self, request, queryset):
+        count = queryset.update(published_date=None)
+        self.message_user(request, f"{count} post(s) despublicado(s) com sucesso.")
+    despublicar_posts.short_description = "Despublicar posts selecionados"
