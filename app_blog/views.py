43583f202b4a1834760fe068bb2ed559d8e889bf
Douglas Silva <douglasitpro@gmail.com>
2025-04-30 00:39:34 -0300

    feat: criando logica para listar posts e adicionando ao templates post_list

--- app_blog/views.py
+++ app_blog/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from . models import Post
+from django.utils import timezone
 
 def post_list(request):
-    return render(request, 'app_blog/post_list.html')
+    
+    # Busca os posts com data de publicação menor ou igual à data e hora atual
+    # e ordena do mais antigo para o mais recente
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+
+    return render(request, 'app_blog/post_list.html', {'posts': posts})
