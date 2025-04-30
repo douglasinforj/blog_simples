5be731b23e47813b365b05ecf75262c0acb83761
Douglas Silva <douglasitpro@gmail.com>
2025-04-30 05:09:44 -0300

    feat: adicionando arquivo de formulário para criação e edição de posts

---
+++ app_blog/forms.py
@@ -0,0 +1,23 @@
+from django import forms 
+from .models import Post
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'text']
+        widgets = {
+            'title': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Digite o título do post',
+            }),
+            'text': forms.Textarea(attrs={
+                'class': 'form-control',
+                'placeholder': 'Digite o conteúdo do post',
+                'rows': 6,
+            }),
+        }
+        labels = {
+            'title': 'Título',
+            'text': 'Conteúdo',
+        }
+        
