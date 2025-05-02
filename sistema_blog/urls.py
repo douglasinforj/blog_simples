b4fc14243ba93c5137c59ceb4aba2dddb4b5d6ea
Douglas Silva <douglasitpro@gmail.com>
2025-05-02 14:50:50 -0300

    adicionando rota para login e logout do django

--- sistema_blog/urls.py
+++ sistema_blog/urls.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app_blog.urls')),
+
+    #autenticao do django:
+
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+
 ]
