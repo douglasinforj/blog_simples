d846ab4e54ce41c3c5fb1d0ea3d741a34a7b3940
Douglas Silva <douglasitpro@gmail.com>
2025-05-05 12:48:34 -0300

    feat: importando signals para o app

--- app_blog/apps.py
+++ app_blog/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class AppBlogConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'app_blog'
+
+    def ready(self):
+        import app_blog.signals
